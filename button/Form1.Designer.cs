eb84e7d030dbe2e95ef482d5731451af7ba754e3
maoukamerasu <maoukamerasu@gmail.com>
2021-09-26 14:40:39 +0900

    プロジェクト ファイルを追加します。

---
+++ button/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿
+namespace button
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button16 = new System.Windows.Forms.Button();
+			this.button17 = new System.Windows.Forms.Button();
+			this.button18 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Gray;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.Location = new System.Drawing.Point(12, 114);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(66, 64);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.click);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(205, 41);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "0";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.label1.UseMnemonic = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Gray;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button2.Location = new System.Drawing.Point(100, 114);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(66, 64);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Gray;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button3.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button3.Location = new System.Drawing.Point(189, 114);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(66, 64);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Gray;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button4.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button4.Location = new System.Drawing.Point(12, 195);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(66, 64);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Gray;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button5.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button5.Location = new System.Drawing.Point(100, 195);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(66, 64);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.click);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Gray;
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button6.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button6.Location = new System.Drawing.Point(189, 195);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(66, 64);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Gray;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button7.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button7.Location = new System.Drawing.Point(12, 275);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(66, 64);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Gray;
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button8.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button8.Location = new System.Drawing.Point(100, 275);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(66, 64);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.click);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.Gray;
+			this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button9.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button9.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button9.Location = new System.Drawing.Point(189, 275);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(66, 64);
+			this.button9.TabIndex = 9;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.click);
+			// 
+			// button10
+			// 
+			this.button10.BackColor = System.Drawing.Color.Gray;
+			this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button10.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button10.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button10.Location = new System.Drawing.Point(12, 356);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(66, 64);
+			this.button10.TabIndex = 10;
+			this.button10.Text = "0";
+			this.button10.UseVisualStyleBackColor = false;
+			this.button10.Click += new System.EventHandler(this.click);
+			// 
+			// button11
+			// 
+			this.button11.BackColor = System.Drawing.Color.Gray;
+			this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button11.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button11.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button11.Location = new System.Drawing.Point(290, 114);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(66, 64);
+			this.button11.TabIndex = 11;
+			this.button11.Text = "+";
+			this.button11.UseVisualStyleBackColor = false;
+			this.button11.Click += new System.EventHandler(this.PlusButton);
+			// 
+			// button12
+			// 
+			this.button12.BackColor = System.Drawing.Color.Gray;
+			this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button12.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button12.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button12.Location = new System.Drawing.Point(290, 195);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(66, 64);
+			this.button12.TabIndex = 12;
+			this.button12.Text = "-";
+			this.button12.UseVisualStyleBackColor = false;
+			this.button12.Click += new System.EventHandler(this.MinusButton);
+			// 
+			// button13
+			// 
+			this.button13.BackColor = System.Drawing.Color.Gray;
+			this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button13.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button13.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button13.Location = new System.Drawing.Point(290, 275);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(66, 145);
+			this.button13.TabIndex = 13;
+			this.button13.Text = "=";
+			this.button13.UseVisualStyleBackColor = false;
+			this.button13.Click += new System.EventHandler(this.EqualButton);
+			// 
+			// button14
+			// 
+			this.button14.BackColor = System.Drawing.Color.Gray;
+			this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button14.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button14.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button14.Location = new System.Drawing.Point(12, 444);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(66, 64);
+			this.button14.TabIndex = 14;
+			this.button14.Text = "*";
+			this.button14.UseVisualStyleBackColor = false;
+			this.button14.Click += new System.EventHandler(this.MultiplicationButton);
+			// 
+			// button15
+			// 
+			this.button15.BackColor = System.Drawing.Color.Gray;
+			this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button15.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button15.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button15.Location = new System.Drawing.Point(189, 356);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(66, 64);
+			this.button15.TabIndex = 15;
+			this.button15.Text = "/";
+			this.button15.UseVisualStyleBackColor = false;
+			this.button15.Click += new System.EventHandler(this.DivisionButton);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Silver;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(19, 46);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(211, 47);
+			this.panel1.TabIndex = 16;
+			// 
+			// button16
+			// 
+			this.button16.BackColor = System.Drawing.Color.Gray;
+			this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button16.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button16.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button16.Location = new System.Drawing.Point(100, 356);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(66, 64);
+			this.button16.TabIndex = 17;
+			this.button16.Text = "・";
+			this.button16.UseVisualStyleBackColor = false;
+			// 
+			// button17
+			// 
+			this.button17.BackColor = System.Drawing.Color.Gray;
+			this.button17.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button17.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button17.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button17.Location = new System.Drawing.Point(279, 46);
+			this.button17.Name = "button17";
+			this.button17.Size = new System.Drawing.Size(77, 46);
+			this.button17.TabIndex = 18;
+			this.button17.Text = "C";
+			this.button17.UseVisualStyleBackColor = false;
+			this.button17.Click += new System.EventHandler(this.ResetButton);
+			// 
+			// button18
+			// 
+			this.button18.BackColor = System.Drawing.Color.Gray;
+			this.button18.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button18.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button18.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button18.Location = new System.Drawing.Point(100, 444);
+			this.button18.Name = "button18";
+			this.button18.Size = new System.Drawing.Size(66, 64);
+			this.button18.TabIndex = 19;
+			this.button18.Text = "√";
+			this.button18.UseVisualStyleBackColor = false;
+			this.button18.Click += new System.EventHandler(this.SquareRoot);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(201, 426);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(132, 102);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 20;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(45)))), ((int)(((byte)(51)))));
+			this.ClientSize = new System.Drawing.Size(376, 540);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button18);
+			this.Controls.Add(this.button17);
+			this.Controls.Add(this.button16);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button15);
+			this.Controls.Add(this.button14);
+			this.Controls.Add(this.button13);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "Form1";
+			this.Text = "電卓";
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button16;
+		private System.Windows.Forms.Button button17;
+		private System.Windows.Forms.Button button18;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ button/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		string value1="0";
+		string value2="0";
+		int calculation = 0;
+		bool reset = false;
+		public string Plus(string value1, string value2) {
+			float label = float.Parse(value1) + float.Parse(value2);
+			return label.ToString();
+		}
+		public string Minus(string value1, string value2)
+		{
+			float label = float.Parse(value1) - float.Parse(value2);
+			return label.ToString();
+		}
+		public string Multiplication(string value1, string value2)
+		{
+			float label = float.Parse(value1) * float.Parse(value2);
+			return label.ToString();
+		}
+		public string Division(string value1, string value2)
+		{
+			float label = float.Parse(value1) / float.Parse(value2);
+			return label.ToString();
+		}
+		private void click(object sender, EventArgs e)
+		{
+			if (float.Parse(label1.Text) != 0&& float.Parse(label1.Text) * 10 + float.Parse(((Button)sender).Text)<99999999&&reset!=true)
+			{
+				float value = float.Parse(label1.Text) * 10 + float.Parse(((Button)sender).Text);
+				label1.Text = value.ToString();
+			}
+			else if(float.Parse(label1.Text) == 0|| reset == true) {
+				label1.Text = ((Button)sender).Text;
+				reset = false;
+			}
+		}
+		private void EqualButton(object ender, EventArgs e) {
+			if (value2 == "0")
+				value2 = label1.Text;
+			else
+				value1 = label1.Text;
+			switch (calculation) {
+				case 1:
+				   label1.Text = Plus(value1, value2);
+				   break;
+				case 2:
+					label1.Text = Minus(value1, value2);
+					break;
+				case 3:
+					label1.Text = Multiplication(value1, value2);
+					break;
+				case 4:
+					label1.Text = Division(value1, value2);
+					break;
+			}
+			reset = true;
+		}
+		private void PlusButton(object ender, EventArgs s) {
+			value1 = label1.Text;
+			calculation = 1;
+			reset = true;
+		}
+		private void MinusButton(object ender, EventArgs s)
+		{
+			value1 = label1.Text;
+			calculation = 2;
+			reset = true;
+		}
+		private void MultiplicationButton(object ender, EventArgs s)
+		{
+			value1 = label1.Text;
+			calculation = 3;
+			reset = true;
+		}
+		private void DivisionButton(object ender, EventArgs s)
+		{
+			value1 = label1.Text;
+			calculation = 4;
+			reset = true;
+		}
+		private void ResetButton(object ender, EventArgs s)
+		{
+			label1.Text = "0";
+			calculation = 0;
+			value1 = "0";
+			value2 = "0";
+		}
+		private void SquareRoot(object ender, EventArgs s) {
+			label1.Text = Math.Sqrt(float.Parse(label1.Text)).ToString();
+			reset = true;
+		}
+	}
+}
